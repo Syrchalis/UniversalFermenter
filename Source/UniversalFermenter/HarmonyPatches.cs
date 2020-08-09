f16e7bbe2adb05f13b6c355f7d2c3c9b85158396
Syrchalis <syrchalis@gmx.de>
2020-08-09 10:33:06 +0200

    Bugfixes

---
+++ Source/UniversalFermenter/HarmonyPatches.cs
@@ -0,0 +1,32 @@
+﻿using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Reflection;
+using RimWorld;
+using Verse;
+using UnityEngine;
+
+namespace UniversalFermenter
+{
+    [StaticConstructorOnStartup]
+    public static class HarmonyPatches
+    {
+        static HarmonyPatches()
+        {
+            var harmony = new Harmony("Syrchalis.Rimworld.UniversalFermenter");
+            harmony.PatchAll(Assembly.GetExecutingAssembly());
+        }
+    }
+    [HarmonyPatch(typeof(Building_FermentingBarrel), nameof(Building_FermentingBarrel.GetInspectString))]
+    public class OldBarrel_GetInspectStringPatch
+    {
+        [HarmonyPrefix]
+        public static bool OldBarrel_GetInspectString_Postfix(ref string __result)
+        {
+            __result = "UF_OldBarrelInspectString".Translate();
+            return false;
+        }
+    }
+}
