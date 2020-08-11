89041d01729d09d8795eb99c43162aff451d2322
Syrchalis <syrchalis@gmx.de>
2020-08-11 03:39:30 +0200

    Language and Settings

--- Source/UniversalFermenter/UF_Core.cs
+++ Source/UniversalFermenter/UF_Core.cs
@@ -28,27 +28,32 @@ namespace UniversalFermenter
                 listing_Standard.Gap(12);
                 listing_Standard.Label("UF_ProcessIconSize".Translate() +  ": " + UF_Settings.processIconSize.ToStringByStyle(ToStringStyle.PercentZero), -1, "UF_ProcessIconSizeTooltip".Translate());
                 UF_Settings.processIconSize = listing_Standard.Slider(GenMath.RoundTo(UF_Settings.processIconSize, 0.05f), 0.2f, 1f);
-                listing_Standard.Gap(12);
-                listing_Standard.CheckboxLabeled("UF_ShowCurrentQualityIcon".Translate(), ref UF_Settings.showCurrentQualityIcon, "UF_ShowCurrentQualityIconTooltip".Translate());
-                listing_Standard.Gap(12);
-                listing_Standard.CheckboxLabeled("UF_ShowTargetQualityIcon".Translate(), ref UF_Settings.showTargetQualityIcon, "UF_ShowTargetQualityTooltip".Translate());
-                listing_Standard.Gap(24);
                 listing_Standard.CheckboxLabeled("UF_SingleItemIcon".Translate(), ref UF_Settings.singleItemIcon, "UF_SingleItemIconTooltip".Translate());
                 listing_Standard.Gap(12);
                 listing_Standard.CheckboxLabeled("UF_SortAlphabetically".Translate(), ref UF_Settings.sortAlphabetically, "UF_SortAlphabeticallyTooltip".Translate());
+                listing_Standard.GapLine(30);
+                listing_Standard.CheckboxLabeled("UF_ShowCurrentQualityIcon".Translate(), ref UF_Settings.showCurrentQualityIcon, "UF_ShowCurrentQualityIconTooltip".Translate());
                 listing_Standard.Gap(12);
-                if (listing_Standard.ButtonText("UF_ReplaceVanillaBarrels".Translate(), "UF_ReplaceVanillaBarrelsTooltip".Translate()))
+                listing_Standard.CheckboxLabeled("UF_ShowTargetQualityIcon".Translate(), ref UF_Settings.showTargetQualityIcon, "UF_ShowTargetQualityTooltip".Translate());
+                listing_Standard.GapLine(30);
+                Rect rectReplaceBarrels = listing_Standard.GetRect(30f);
+                TooltipHandler.TipRegion(rectReplaceBarrels, "UF_ReplaceVanillaBarrelsTooltip".Translate());
+                if (Widgets.ButtonText(rectReplaceBarrels, "UF_ReplaceVanillaBarrels".Translate(), true, true, true))
                 {
                     ReplaceVanillaBarrels();
                 }
-                listing_Standard.Gap(24);
-                if (listing_Standard.ButtonText("UF_DefaultSettings".Translate(), "UF_DefaultSettingsTooltip".Translate()))
+                listing_Standard.GapLine(30);
+                Rect rectDefaultSettings = listing_Standard.GetRect(30f);
+                TooltipHandler.TipRegion(rectDefaultSettings, "UF_DefaultSettingsTooltip".Translate());
+                if (Widgets.ButtonText(rectDefaultSettings, "UF_DefaultSettings".Translate(), true, true, true))
                 {
                     UF_Settings.showProcessIconGlobal = true;
                     UF_Settings.processIconSize = 0.6f;
                     UF_Settings.singleItemIcon = true;
                     UF_Settings.sortAlphabetically = false;
-    }
+                    UF_Settings.showCurrentQualityIcon = true;
+                    UF_Settings.showTargetQualityIcon = false;
+                }
                 listing_Standard.End();
                 settings.Write();
             }
@@ -104,7 +109,7 @@ namespace UniversalFermenter
                         compUF.ProgressTicks = (int)(360000 * progress);
                     }
                 }
-                foreach (Thing thing in map.listerThings.ThingsOfDef(ThingDefOf.MinifiedThing).Where(t => t.GetInnerIfMinified().def == ThingDefOf.FermentingBarrel).ToList())
+                foreach (Thing thing in map.listerThings.ThingsOfDef(ThingDefOf.MinifiedThing).Where(t => t.GetInnerIfMinified().def == ThingDefOf.FermentingBarrel))
                 {
                     MinifiedThing minifiedThing = thing as MinifiedThing;
                     ThingDef stuff;
